f0dab47dd65ec25775d0a924918f879344bf37a9
sumanta-ghosh <sumanta.ghosh.mit@gmail.com>
2020-01-01 02:21:58 +0530

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <style>
+		body{background-image:linear-gradient(-90deg, #7a446e, #00070a);}
+		.responsive{
+  max-width:100%;
+  max-height:100%;
+}
+
+.center{
+  position: absolute;
+  top: 50%;
+  left: 50%; 
+  transform: translate(-50%, -50%);
+}
+
+parent{
+  position: relative;
+}
+        </style>
+    </head>
+    <body>
+	<div class="parrent"> 
+      <img class="responsive center" src="quizomania.jpeg">
+
+</div>
+
+    </body>
+</html>
